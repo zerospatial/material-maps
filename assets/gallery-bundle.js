37c5af410da1c3b06ff12a69c9a39cc914625838
malcolm <malcolmtmeyer@gmail.com>
2017-10-08 21:47:43 -0400

    update

---
+++ assets/gallery-bundle.js
@@ -0,0 +1,4 @@
+---
+---
+{% include assets/jquery-3.1.1.min.js %}
+{% include assets/material-dashboard/material.min.js %}
---
+++ assets/map-bundle.js
@@ -0,0 +1,34 @@
+---
+---
+{% include assets/jquery-3.1.1.min.js %}
+{% include assets/material-dashboard/material.min.js %}
+{% include node_modules/leaflet/dist/leaflet.js %}
+{% include assets/Leaflet.fullscreen.min.js %}
+{% include assets/leaflet-material/leaflet-material-custom.js %}
+{% include assets/leaflet-search/leaflet-search.src.js %}
+{% include assets/leaflet-hash.js %}
+{% include assets/leaflet-omnivore.min.js %}
+/*
+* Reveal custom layer control
+*/
+$(".md-reveal--title").on('click', function(e) {
+  console.log(e);
+  var v = e.target.value;
+  if (v < 1) {
+    console.log(e.target.parentElement["aria-expanded"])
+    var x = e.target.parentElement.children[1];
+    console.log(x);
+    var ht = x.scrollHeight;
+    x.style.height =  ht + "px";
+    e.target.parentElement["aria-expanded"] = true;
+    e.target.parentElement.children[0].value = 1;
+    return false
+  }
+  if (v > 0) {
+    var x = e.target.parentElement.children[1];
+    var ht = x.scrollHeight;
+    x.style.height =  0;
+    e.target.parentElement["aria-expanded"] = false;
+    e.target.parentElement.children[0].value = 0;
+  }
+});
