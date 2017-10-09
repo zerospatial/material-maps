4ce48bc04a3a45a7d990c6a1e5d895518db29ddc
reyemtm <malcolmtmeyer@gmail.com>
2017-10-09 09:18:21 -0400

    fix zoom

--- assets/map-bundle.js
+++ assets/map-bundle.js
@@ -3,7 +3,6 @@
 {% include assets/jquery-3.1.1.min.js %}
 {% include node_modules/leaflet/dist/leaflet.js %}
 {% include assets/leaflet-omnivore.min.js %}
-{% include assets/Leaflet.fullscreen.min.js %}
 {% include assets/material-dashboard/material.min.js %}
 {% include assets/leaflet-fullscreen/Control.FullScreen.js %}
 {% include assets/leaflet-search/leaflet-search.src.js %}
