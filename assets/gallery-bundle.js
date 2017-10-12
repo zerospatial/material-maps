e43560593d9fddda5d70b1242cfcef200145018e
reyemtm <malcolmtmeyer@gmail.com>
2017-10-12 16:04:35 -0400

    update

--- assets/gallery-bundle.js
+++ assets/gallery-bundle.js
@@ -1,4 +1,14 @@
 ---
+layout: compress
 ---
 {% include assets/jquery-3.1.1.min.js %}
 {% include assets/material-dashboard/material.min.js %}
+$('.filter').click(function() {
+  console.log('test');
+  var id = $(this).attr('id');
+  $('.' + id).fadeIn();
+  $('.app').not('.' + id).hide();
+});
+$('#reset').click(function() {
+  $('.app').fadeIn();
+});
