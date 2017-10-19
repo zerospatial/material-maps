fedab883ecd8b3c9b93b1d0488e5e8c8b798314a
malcolm <malcolmtmeyer@gmail.com>
2017-10-18 21:55:10 -0400

    reveal--icons

--- assets/map-bundle.js
+++ assets/map-bundle.js
@@ -8,27 +8,4 @@
 {% include assets/leaflet-search/leaflet-search.src.js %}
 {% include assets/leaflet-hash.js %}
 {% include assets/Leaflet.VectorGrid.bundled.min.js %}
-/*
-* Reveal custom layer control
-*/
-$(".md-reveal--title").on('click', function(e) {
-  console.log(e);
-  var v = e.target.value;
-  if (v < 1) {
-    console.log(e.target.parentElement["aria-expanded"])
-    var x = e.target.parentElement.children[1];
-    console.log(x);
-    var ht = x.scrollHeight;
-    x.style.height =  ht + "px";
-    e.target.parentElement["aria-expanded"] = true;
-    e.target.parentElement.children[0].value = 1;
-    return false
-  }
-  if (v > 0) {
-    var x = e.target.parentElement.children[1];
-    var ht = x.scrollHeight;
-    x.style.height =  0;
-    e.target.parentElement["aria-expanded"] = false;
-    e.target.parentElement.children[0].value = 0;
-  }
-});
+{% include assets/md-reveal.js %}
